41a304f7442afba7317925df8af55575211a17c5
Amol Tivarekar <amoltivarekar@gmail.com>
2020-06-10 11:12:49 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 500, function(){
         window.location.hash = hash;
       });
     } 
